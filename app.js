42c150f264fc517a4604309ffc37c239de0018f7
Lena <elenazyamileva@gmail.com>
2024-05-01 22:01:07 +0300

    commit

---
+++ app.js
@@ -0,0 +1,42 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+// import authRouter from "./routes/authRouter.js";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+// app.use("/api/users", authRouter);
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(PORT, () => {
+      console.log(`Server is running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error.message);
+    process.exit(1);
+  });
+
+export default app;
