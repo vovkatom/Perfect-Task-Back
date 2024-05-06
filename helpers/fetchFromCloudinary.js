ee8c16e4d34be059e31a1545ce0b79bb4cf653e3
Taras <topanasenko85@gmail.com>
2024-05-06 19:01:52 +0300

    fix fetchFromCloudinary by tag

--- helpers/fetchFromCloudinary.js
+++ helpers/fetchFromCloudinary.js
@@ -7,39 +7,17 @@ const fetchFromCloudinary = async (tag) => {
 
   if (tag !== "no-background") {
     try {
-      const folderName = `backgrounds/${tag}`;
-      // const folderName = `backgrounds`;
-      console.log("folderName:", folderName);
-
-      const result = await cloudinary.api.resources({
-        type: "upload",
-        prefix: folderName,
-      });
-
-      if (result.resources && result.resources.length > 0) {
-        const nonZeroByteFiles = result.resources.filter(
-          (file) => file.bytes > 0
-        );
-
-        const sortedImg = result.resources.sort((a, b) => a.width - b.width);
-        // const sortedImg = nonZeroByteFiles.sort((a, b) => a.width - b.width);
-        const setOfImages = {
-          // min_1x: sortedImg[0].secure_url,
-          // min_2x: sortedImg[1].secure_url,
-          mobile_1x: sortedImg[2].secure_url,
-          mobile_2x: sortedImg[3].secure_url,
-          tablet_1x: sortedImg[4].secure_url,
-          tablet_2x: sortedImg[6].secure_url,
-          desktop_1x: sortedImg[5].secure_url,
-          desktop_2x: sortedImg[7].secure_url,
-        };
-        console.log(sortedImg.length);
-        console.log(result);
-
-        return setOfImages;
-      } else {
-        return;
-      }
+      const result = await cloudinary.api.resources_by_tag(tag);
+      const sortedImg = result.resources.sort((a, b) => a.width - b.width);
+      const setOfImages = {
+        mobile_1x: sortedImg[0].secure_url,
+        mobile_2x: sortedImg[1].secure_url,
+        tablet_1x: sortedImg[2].secure_url,
+        tablet_2x: sortedImg[4].secure_url,
+        desktop_1x: sortedImg[3].secure_url,
+        desktop_2x: sortedImg[5].secure_url,
+      };
+      return setOfImages;
     } catch (error) {
       console.error(error);
     }
